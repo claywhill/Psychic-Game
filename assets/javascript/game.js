9536c505a72290136ac3441b9f16a05831774f49
Clayton Hill <clayman0123@yahoo.com>
2020-02-18 12:36:22 -0700

    computer now chooses new letter with each win/loss and alerts user of correct letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,23 +27,32 @@ var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 document.onkeydown = function() {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+// conditional statement that checks if the user guess is equal to the computer choice
+// if they are equal then the computer will alert the user of the correct letter, wins will increase by 1, the letters guessed will reset, guesses remaining will be reset to 10, and the computer will choose a new letter 
     if (userGuess==computerChoice) {
         alert("The correct letter was " + computerChoice.toUpperCase() + "!");
         wins++;
         lettersUsed = [];
-        guessesRemaining = 10;    
-    } else if (userGuess!==computerChoice) {
+        guessesRemaining = 10;
+        computerChoice = letters[Math.floor(Math.random() * letters.length)];
+
+// if the user guess does not equal computer choice then guesses remaining will decrease by 1 and the user guess will be displayed in the used letters
+    } else {
         guessesRemaining--;
         lettersUsed.push(userGuess);
+
+// if the number of guesses remaining drops to 0 then the computer will alert the user of the correct letter, losses will increase by 1, letters gussed will reset, gusses remaining will be reset to 10, and teh computer will choose a new letter
     }   if (guessesRemaining < 1) {
+        alert("Sorry, the correct letter was " + computerChoice.toUpperCase() + ". Better luck next time.");
         losses++;
         lettersUsed = [];
-        guessesRemaining = 10;    
+        guessesRemaining = 10;
+        computerChoice = letters[Math.floor(Math.random() * letters.length)];
+
     }
-
-    
-
+// variable that stores the html that will be updated to the screen once the conditional statement runs
     var html = "<h1>Psychic Game</h1>" + "<p>Guess what letter I'm thinking of</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + guessesRemaining + "</p>" + "<p>Your Guesses So Far: " + lettersUsed + "</p>";
 
     document.querySelector("#game").innerHTML = html;
 }
+
