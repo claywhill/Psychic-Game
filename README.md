30f045fd657693c3479a5f3ae6079b19522ee454
Clayton Hill <clayman0123@yahoo.com>
2020-02-19 16:33:50 -0700

    commit for homework submission

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Psychic-Game
+This is the Psychic Game. In this game, the user must try to guess which letter the computer has chosen. The user is given 10 tries to guess the letter. If the user is able to guess the letter within those 10 tries, they win. If the user fails to guess the letter within those 10 tries, they lose. The computer will help the user keep track of their guesses by displaying them under the user's stats. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,17 +14,17 @@ var wins = 0;
 // variable for storing number of losses
 var losses = 0;
 
-// variable for displaying letters already guessed 
+// variable for storing letters already guessed 
 var lettersUsed = [];
 
-// variable for displaying number of guesses user has left
+// variable for storing number of guesses user has left
 var guessesRemaining = 10;
 
-// radomizes the letters computer can choose
+// variable for store the radomized letter the computer has chosen
 var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 
 // function for capturing the key pressed by user
-document.onkeydown = function() {
+document.onkeydown = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 // conditional statement that checks if the user guess is equal to the computer choice
@@ -41,18 +41,18 @@ document.onkeydown = function() {
         guessesRemaining--;
         lettersUsed.push(userGuess);
 
-// if the number of guesses remaining drops to 0 then the computer will alert the user of the correct letter, losses will increase by 1, letters gussed will reset, gusses remaining will be reset to 10, and teh computer will choose a new letter
-    }   if (guessesRemaining < 1) {
+// if the number of guesses remaining drops to 0 then the computer will alert the user of the correct letter, losses will increase by 1, letters guessed will reset, guesses remaining will be reset to 10, and the computer will choose a new letter
+    } if (guessesRemaining < 1) {
         alert("Sorry, the correct letter was " + computerChoice.toUpperCase() + ". Better luck next time.");
         losses++;
         lettersUsed = [];
         guessesRemaining = 10;
         computerChoice = letters[Math.floor(Math.random() * letters.length)];
-
     }
+
 // variable that stores the html that will be updated to the screen once the conditional statement runs
     var html = "<h1>Psychic Game</h1>" + "<p>Guess what letter I'm thinking of</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + guessesRemaining + "</p>" + "<p>Your Guesses So Far: " + lettersUsed + "</p>";
 
-    document.querySelector("#game").innerHTML = html;
+// prints the html variable to the screen in the div that matches the id "game"
+    document.getElementById("game").innerHTML = html;
 }
-
